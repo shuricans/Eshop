3e2d42b53c75330c7954de3ae066c48b61ed0bfa
Sasha Karachev <shuricans@gmail.com>
2022-02-28 00:30:22 +0300

    angular components

--- shop-frontend-app/src/app/app.module.ts
+++ shop-frontend-app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductGalleryPageComponent } from './page/product-gallery-page/product-gallery-page.component';
+import { ProductInfoPageComponent } from './page/product-info-page/product-info-page.component';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { ProductFilterComponent } from './component/product-filter/product-filter.component';
+import { ProductPaginationComponent } from './component/product-pagination/product-pagination.component';
+import { ProductGalleryComponent } from './component/product-gallery/product-gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductGalleryPageComponent,
+    ProductInfoPageComponent,
+    NavBarComponent,
+    ProductFilterComponent,
+    ProductPaginationComponent,
+    ProductGalleryComponent
   ],
   imports: [
     BrowserModule,
---
+++ shop-frontend-app/src/app/page/product-gallery-page/product-gallery-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-gallery-page',
+  templateUrl: './product-gallery-page.component.html',
+  styleUrls: ['./product-gallery-page.component.scss']
+})
+export class ProductGalleryPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
