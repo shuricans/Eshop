4f8f4b28d1296819516d09e4f2e9b3a61558cbba
Sasha Karachev <shuricans@gmail.com>
2022-03-21 12:24:01 +0300

    homework 4

--- shop-frontend-app/src/app/component/product-filter/product-filter.component.ts
+++ shop-frontend-app/src/app/component/product-filter/product-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-product-filter',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductFilterComponent implements OnInit {
 
+  nameFilter: string = '';
+
+  @Output() applyFilterEvent = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  applyFilter() {
+    this.applyFilterEvent.emit(this.nameFilter);
   }
 
 }
--- shop-frontend-app/src/app/page/product-gallery-page/product-gallery-page.component.ts
+++ shop-frontend-app/src/app/page/product-gallery-page/product-gallery-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../../service/product.service";
+import {Product} from "../../model/product";
+import {Page} from "../../model/page";
 
 @Component({
   selector: 'app-product-gallery-page',
@@ -7,9 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductGalleryPageComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
+
+  page?: Page;
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.productService.findAll()
+      .subscribe({
+        next: res => {
+          console.log('Loading products...')
+          this.page = res;
+          this.products = res.content;
+        },
+        error: err => {
+          console.error(`Error loading products ${err}`);
+        }
+      })
   }
 
+  goToPage(page: number) {
+    this.productService.findAll(page)
+      .subscribe({
+        next: res => {
+          console.log('Loading products...')
+          this.page = res;
+          this.products = res.content;
+        },
+        error: err => {
+          console.error(`Error loading products ${err}`);
+        }
+      })
+  }
+
+  applyFilter(nameFilter: string) {
+    this.productService.findAll(1, nameFilter)
+      .subscribe({
+        next: res => {
+          console.log('Loading products...')
+          this.page = res;
+          this.products = res.content;
+        },
+        error: err => {
+          console.error(`Error loading products ${err}`);
+        }
+      })
+  }
 }
