622a705609f52b1d4a32c9a596f1e3667aebe7e4
Sasha Karachev <shuricans@gmail.com>
2022-03-24 15:33:26 +0300

    CartService, Session Redis

---
+++ shop-backend-api-app/src/main/java/ru/geekbrains/controller/CartController.java
@@ -0,0 +1,49 @@
+package ru.geekbrains.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import ru.geekbrains.service.CartService;
+import ru.geekbrains.service.ProductService;
+import ru.geekbrains.service.dto.AddLineItemDto;
+import ru.geekbrains.service.dto.AllCartDto;
+import ru.geekbrains.service.dto.LineItem;
+import ru.geekbrains.service.dto.ProductDto;
+
+import java.util.List;
+
+@Tag(name = "Cart", description = "Service to get carts information")
+@AllArgsConstructor
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CartController.class);
+
+    private final CartService cartService;
+
+    private final ProductService productService;
+
+    @PostMapping(produces = "application/json", consumes = "application/json")
+    public List<LineItem> addToCart(@RequestBody AddLineItemDto addLineItemDto) {
+        logger.info("New LineItem. ProductId = {}, qty = {}", addLineItemDto.getProductId(), addLineItemDto.getQty());
+
+        ProductDto productDto = productService.findById(addLineItemDto.getProductId())
+                .orElseThrow(RuntimeException::new);
+        cartService.addProductQty(productDto, addLineItemDto.getColor(), addLineItemDto.getMaterial(), addLineItemDto.getQty());
+        return cartService.getLineItems();
+    }
+
+    @DeleteMapping(consumes = "application/json")
+    public void deleteLineItem(@RequestBody LineItem lineItem) {
+        cartService.removeProduct(lineItem.getProductDto(), lineItem.getColor(), lineItem.getMaterial());
+    }
+
+    @GetMapping("/all")
+    public AllCartDto findAll() {
+        return new AllCartDto(cartService.getLineItems(), cartService.getSubTotal());
+    }
+}
+
