ef454f6529b9218b2b777bd92a34a7afd390773f
Sasha Karachev <shuricans@gmail.com>
2022-04-10 16:01:51 +0300

    homework 9

--- shop-admin-app/src/main/jib/entrypoint.sh
+++ shop-admin-app/src/main/jib/entrypoint.sh
@@ -3,16 +3,40 @@
 # Abort on any error (including if wait-for fails).
 set -e
 
-/wait-for-service.sh "$EUREKA_HOST" "$EUREKA_PORT"
-/wait-for-service.sh "$CONFIG_HOST" "$CONFIG_PORT"
-/wait-for-service.sh "$DB_HOST" "$DB_PORT"
-/wait-for-service.sh "$GATEWAY_HOST" "$GATEWAY_PORT"
+args=""
 
-serviceUrl="http://$EUREKA_HOST:$EUREKA_PORT/eureka/"
-configUri="http://$CONFIG_HOST:$CONFIG_PORT"
+# Waiting eureka service if exist.
+if [ -n "$PROFILE" ]; then
+  args="$args --spring.profiles.active=$PROFILE"
+fi
+
+# Waiting eureka service if exist.
+if [ -n "$EUREKA_HOST" ] && [ -n "$EUREKA_PORT" ]; then
+  /wait-for-service.sh "$EUREKA_HOST" "$EUREKA_PORT"
+  serviceUrl="http://$EUREKA_HOST:$EUREKA_PORT/eureka/"
+  args="$args --eureka.client.serviceUrl.defaultZone=$serviceUrl"
+else
+  args="$args --eureka.client.enabled=false"
+fi
+
+# Waiting cloud-config service if exist.
+if [ -n "$CONFIG_HOST" ] && [ -n "$CONFIG_PORT" ]; then
+  /wait-for-service.sh "$CONFIG_HOST" "$CONFIG_PORT"
+  configUri="http://$CONFIG_HOST:$CONFIG_PORT"
+  args="$args --spring.cloud.config.uri=$configUri"
+fi
+
+# Waiting db if exist.
+if [ -n "$DB_HOST" ] && [ -n "$DB_PORT" ]; then
+  /wait-for-service.sh "$DB_HOST" "$DB_PORT"
+fi
+
+# Waiting gateway service if exist.
+if [ -n "$GATEWAY_HOST" ] && [ -n "$GATEWAY_PORT" ]; then
+  /wait-for-service.sh "$GATEWAY_HOST" "$GATEWAY_PORT"
+fi
 
 exec java -cp \
-$( cat /app/jib-classpath-file ) \
-$( cat /app/jib-main-class-file ) \
---eureka.client.serviceUrl.defaultZone="$serviceUrl" \
---spring.cloud.config.uri="$configUri"
+  $(cat /app/jib-classpath-file) \
+  $(cat /app/jib-main-class-file) \
+  $args
